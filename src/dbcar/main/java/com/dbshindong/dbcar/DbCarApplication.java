1fb70976ad124845721734504637bcef3dc7e9ac
yeo-li <parkseongyeol2110@gmail.com>
2025-05-15 16:29:39 +0900

    Initial commit

---
+++ src/dbcar/main/java/com/dbshindong/dbcar/DbCarApplication.java
@@ -0,0 +1,9 @@
+package dbcar.main.java.com.dbshindong.dbcar;
+
+public class DbCarApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("빌드 후 오류가 생긴다면 말해주세요");
+	}
+}
---
+++ src/dbcar/main/java/com/dbshindong/dbcar/infrastructure/DBConnection.java
@@ -0,0 +1,5 @@
+package dbcar.main.java.com.dbshindong.dbcar.infrastructure;
+
+public class DBConnection {
+	// todo: db 연결 메서드 작성
+}
