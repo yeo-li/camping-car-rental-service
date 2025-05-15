203b6c4e68f3c0a1301b30d55e2242e83845abc8
yeo-li <parkseongyeol2110@gmail.com>
2025-05-15 21:13:28 +0900

    feat : 데이터베이스 초기화 기능 구현(#1)
    초기화 sql이 위치한 파일 경로를 입력하면 root/1234 관리자 게정으로 접속하여 데이터베이스를 초기화 합니다.

---
+++ src/dbcar/main/java/com/dbshindong/dbcar/application/DatabaseInitService.java
@@ -0,0 +1,35 @@
+package dbcar.main.java.com.dbshindong.dbcar.application;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.io.*;
+import java.sql.*;
+
+import dbcar.main.java.com.dbshindong.dbcar.infrastructure.DBConnection;
+
+public class DatabaseInitService {
+	public void initDatabase(String resourcePath) {
+		try(Connection conn = DBConnection.getConnection("root", "1234")) {
+			InputStream is = DatabaseInitService.class
+					.getClassLoader()
+					.getResourceAsStream(resourcePath);
+			if(is == null) {
+				throw new RuntimeException("[initDatabase] sql 파일을 찾을 수 없습니다. (" + resourcePath +")");
+			}
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            Statement stmt = conn.createStatement();
+            
+            String sql = reader.lines().collect(Collectors.joining("\n"));
+            for (String command : sql.split(";")) {
+                if (!command.strip().isEmpty()) {
+                    stmt.execute(command.trim() + ";");
+                }
+            }
+            System.out.println("[initDatabase] 데이터베이스 초기화 완료");
+            
+		} catch(SQLException e) {
+			System.err.println("[initDatabase] SQL 스크립트 실행 중 오류 발생");
+			e.printStackTrace();
+		}
+	}
+}
