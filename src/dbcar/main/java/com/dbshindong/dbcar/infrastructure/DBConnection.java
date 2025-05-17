72eb250f2574b82e2972bbc9af44a80a7f74a735
Sunwoo <Administrator@swkim-ntb>
2025-05-17 17:58:04 +0900

    feat : 데이터베이스 연결 구현

--- src/dbcar/main/java/com/dbshindong/dbcar/infrastructure/DBConnection.java
+++ src/dbcar/main/java/com/dbshindong/dbcar/infrastructure/DBConnection.java
@@ -1,5 +1,27 @@
 package dbcar.main.java.com.dbshindong.dbcar.infrastructure;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
-	// todo: db 연결 메서드 작성
+	
+	public static Connection conn;
+	public static void main (String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); // MySQL 드라이버 로드
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/DBTEST", "root","1234"); // JDBC 연결
+			System.out.println("DB 연결 완료");
+		} catch (ClassNotFoundException e) {
+			System.out.println("JDBC 드라이버 로드 오류");
+			System.out.println(e);
+		} catch (SQLException e) {
+			System.out.println("DB 연결 오류");
+		}
+		
+	}
+	public static Connection getConnection() {
+		return conn;
+	}
+	
 }
