40f463491071696cf1042f3379a817ec0799af42
yeo-li <parkseongyeol2110@gmail.com>
2025-05-20 15:53:40 +0900

    refactor : AssertUtil 값 비교방법 변경(#19)
    이전코드는 expected.equals(actual) 이었지만,
    expect가 null 일때 NullPointerException이 발생하여
    Objects.equals(expected, actual)로 변경했습니다.

--- src/dbcar/main/java/com/dbshindong/dbcar/common/AssertUtil.java
+++ src/dbcar/main/java/com/dbshindong/dbcar/common/AssertUtil.java
@@ -1,8 +1,10 @@
 package dbcar.main.java.com.dbshindong.dbcar.common;
 
+import java.util.Objects;
+
 public class AssertUtil {
     public static void assertEqual(Object expected, Object actual, String message) {
-        if (!expected.equals(actual)) {
+        if (!Objects.equals(expected, actual)) {
             System.out.println("❌ 실패: " + message + " (expected=" + expected + ", actual=" + actual + ")");
         } else {
             System.out.println("✅ 성공: " + message);
