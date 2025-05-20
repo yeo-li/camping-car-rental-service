47fef5eb5efe8cb49519579e2b9ad614f566c196
yeo-li <parkseongyeol2110@gmail.com>
2025-05-21 00:36:00 +0900

    feat : CampingCarCompany 도메인 구현(#19)

---
+++ src/dbcar/main/java/com/dbshindong/dbcar/domain/company/CampingCarCompany.java
@@ -0,0 +1,77 @@
+package dbcar.main.java.com.dbshindong.dbcar.domain.company;
+
+import java.util.Objects;
+
+public class CampingCarCompany {
+	private final int company_id;
+	private final String name;
+	private final String address;
+	private final String phone;
+	private final String manager_name;
+	private final String manager_email;
+
+	private static final String NULL_MESSAGE = "%s은(는) null이 들어갈 수 없습니다.";
+
+	public CampingCarCompany(int company_id, String name, String address, String phone, String manager_name,
+			String manager_email) {
+		this.validate(company_id, name, address, phone, manager_name, manager_email);
+		this.company_id = company_id;
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+		this.manager_name = manager_name;
+		this.manager_email = manager_email;
+	}
+
+	public CampingCarCompany(String name, String address, String phone, String manager_name, String manager_email) {
+		this.validate(-1, name, address, phone, manager_name, manager_email);
+		this.company_id = -1;
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+		this.manager_name = manager_name;
+		this.manager_email = manager_email;
+	}
+
+	private void validate(int company_id, String name, String address, String phone, String manager_name,
+			String manager_email) {
+		Objects.requireNonNull(company_id, String.format(NULL_MESSAGE, "company_id"));
+		Objects.requireNonNull(name, String.format(NULL_MESSAGE, "name"));
+		Objects.requireNonNull(address, String.format(NULL_MESSAGE, "address"));
+		Objects.requireNonNull(phone, String.format(NULL_MESSAGE, "phone"));
+		Objects.requireNonNull(manager_name, String.format(NULL_MESSAGE, "manager_name"));
+		Objects.requireNonNull(manager_email, String.format(NULL_MESSAGE, "manager_email"));
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"{ \"company_id\": %d, \"name\": \"%s\", \"address\": \"%s\", \"phone\": \"%s\", "
+						+ "\"manager_name\": \"%s\", \"manager_email\": \"%s\" }",
+				company_id, name, address, phone, manager_name, manager_email);
+	}
+
+	public int getCompany_id() {
+		return company_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getManager_name() {
+		return manager_name;
+	}
+
+	public String getManager_email() {
+		return manager_email;
+	}
+}
