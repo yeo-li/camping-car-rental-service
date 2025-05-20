67dc9f902f6703ceb80d3dfbcfad844e57e4d33a
yeo-li <parkseongyeol2110@gmail.com>
2025-05-20 13:23:49 +0900

    style : CustomerRepository.java 코드 정리(#19)

--- src/dbcar/main/java/com/dbshindong/dbcar/infrastructure/customer/CustomerRepository.java
+++ src/dbcar/main/java/com/dbshindong/dbcar/infrastructure/customer/CustomerRepository.java
@@ -7,21 +7,21 @@ import dbcar.main.java.com.dbshindong.dbcar.domain.customer.Customer;
 
 public class CustomerRepository {
 	private final Connection conn;
-	
+
 	public CustomerRepository(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	public Customer findById(int id) {
-		
+
 		Customer customer = null;
 		try {
 			String sql = "SELECT * FROM Customer where customer_id = ?";
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, id);
-			
+
 			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				int customer_id = rs.getInt("customer_id");
 				String username = rs.getString("username");
 				String password = rs.getString("password");
@@ -30,28 +30,28 @@ public class CustomerRepository {
 				String address = rs.getString("address");
 				String phone = rs.getString("phone");
 				String email = rs.getString("email");
-				
+
 				customer = new Customer(customer_id, username, password, license_number, name, address, phone, email);
 			}
-			
-		} catch(SQLException e) {
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
 		}
-		
+
 		return customer;
-		
+
 	}
-	
+
 	public List<Customer> findAll() {
 		List<Customer> customers = new ArrayList<>();
-		
+
 		try {
 			String sql = "SELECT * FROM Customer";
 			PreparedStatement pstmt = conn.prepareStatement(sql);
-			
+
 			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				int customer_id = rs.getInt("customer_id");
 				String username = rs.getString("username");
 				String password = rs.getString("password");
@@ -60,31 +60,33 @@ public class CustomerRepository {
 				String address = rs.getString("address");
 				String phone = rs.getString("phone");
 				String email = rs.getString("email");
-				
-				Customer customer = new Customer(customer_id, username, password, license_number, name, address, phone, email);
+
+				Customer customer = new Customer(customer_id, username, password, license_number, name, address, phone,
+						email);
 				customers.add(customer);
 			}
-			
-		} catch(SQLException e) {
+
+		} catch (SQLException e) {
 			e.printStackTrace();
-		} catch(NullPointerException e) {
+		} catch (NullPointerException e) {
 			e.printStackTrace();
 		}
-		
+
 		return customers;
 	}
-	
+
 	public void deleteById(int id) {
 		String sql = "DELETE FROM Customer WHERE customer_id = ?";
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, id);
-			
+
 			int deleted = pstmt.executeUpdate();
-			if(deleted > 0) System.out.printf("%d개 삭제 되었습니다.\n", deleted);
-		} catch(SQLException e) {
+			if (deleted > 0)
+				System.out.printf("%d개 삭제 되었습니다.\n", deleted);
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
