5ff6df6ffc66c790726d2500aefaba931442ae20
yeo-li <parkseongyeol2110@gmail.com>
2025-05-22 14:13:44 +0900

    refactor : DatabaseInsertService create{domain}()메서드명 수정(#5)
    creat{domain}() -> create{domain}() 으로 수정했습니다.

--- src/dbcar/main/java/com/dbshindong/dbcar/application/DataInsertService.java
+++ src/dbcar/main/java/com/dbshindong/dbcar/application/DataInsertService.java
@@ -64,7 +64,7 @@ public class DataInsertService {
 		campingCarCampanyRepository.save(company);
 	}
 
-	public CampingCar creatCampingCar(String name, String plate_number, int capacity, byte[] image, String description,
+	public CampingCar createCampingCar(String name, String plate_number, int capacity, byte[] image, String description,
 			int rental_price, int company_id, Date registered_date) {
 		try {
 			CampingCar car = new CampingCar(name, plate_number, capacity, image, description, rental_price, company_id,
@@ -80,7 +80,7 @@ public class DataInsertService {
 		campingCarRepository.save(car);
 	}
 
-	public Employee creatEmployee(String name, String phone, String address, int salary, int dependents,
+	public Employee createEmployee(String name, String phone, String address, int salary, int dependents,
 			String department, String role) {
 		try {
 			Employee employee = new Employee(name, phone, address, salary, dependents, department, role);
@@ -95,7 +95,7 @@ public class DataInsertService {
 		employeeRepository.save(employee);
 	}
 
-	public Customer creatCustomer(String username, String password, String license_number, String name, String address,
+	public Customer createCustomer(String username, String password, String license_number, String name, String address,
 			String phone, String email) {
 		try {
 			Customer customer = new Customer(username, password, license_number, name, address, phone, email);
@@ -110,7 +110,7 @@ public class DataInsertService {
 		customerRepository.save(customer);
 	}
 
-	public Rental creatRental(int car_id, int customer_id, int company_id, Date start_date, int rental_period,
+	public Rental createRental(int car_id, int customer_id, int company_id, Date start_date, int rental_period,
 			int total_charge, Date due_date, String extra_charge_detail, Integer extra_charge) {
 		try {
 			Rental rental = new Rental(car_id, customer_id, company_id, start_date, rental_period, total_charge,
@@ -126,7 +126,7 @@ public class DataInsertService {
 		rentalRepository.save(rental);
 	}
 
-	public ExternalRepairRecord creatExternalRepairRecord(int car_id, int shop_id, int company_id, int customer_id,
+	public ExternalRepairRecord createExternalRepairRecord(int car_id, int shop_id, int company_id, int customer_id,
 			String content, Date repair_date, int cost, Date due_date, String note) {
 		try {
 			ExternalRepairRecord record = new ExternalRepairRecord(car_id, shop_id, company_id, customer_id, content,
@@ -142,7 +142,7 @@ public class DataInsertService {
 		externalRepairRecordRepository.save(record);
 	}
 
-	public ExternalRepairShop creatExternalRepairShop(String name, String address, String phone, String manager_name,
+	public ExternalRepairShop createExternalRepairShop(String name, String address, String phone, String manager_name,
 			String manager_email) {
 		try {
 			ExternalRepairShop shop = new ExternalRepairShop(name, address, phone, manager_name, manager_email);
