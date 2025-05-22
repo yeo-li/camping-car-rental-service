69c4ad0cdc0f7c6706dab1757bd4a8bbe264e123
Sunwoo <Administrator@172.18.0.125>
2025-05-22 16:54:11 +0900

    fix : 잘못된 경로 수정 / feat : user eventlistener 추가 (#20)

--- src/dbcar/main/java/com/dbshindong/dbcar/ui/view/LoginView.java
+++ src/dbcar/main/java/com/dbshindong/dbcar/ui/view/LoginView.java
@@ -1,8 +1,16 @@
-package ui.view;
+package dbcar.main.java.com.dbshindong.dbcar.ui.view;
 import javax.swing.*;
+import dbcar.main.java.com.dbshindong.dbcar.ui.controller.LoginController;
 
 public class LoginView {
-	public static void main(String[] args) {
+	private LoginController loginController;
+	
+	public LoginView(LoginController loginController) {
+		this.loginController = loginController;
+		createUI();
+	}
+	
+	public void createUI() {
 		JFrame frame = new JFrame("Login");//초기 화면
 		frame.setSize(800,600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,7 +25,7 @@ public class LoginView {
 		
 	}
 
-	private static void addLoginComponent(JPanel panel) {
+	private void addLoginComponent(JPanel panel) {
 		
 		int presety = (600-145) / 2;
 		int presetx = (800- 220) / 2 ;
@@ -46,6 +54,19 @@ public class LoginView {
 		JButton adminLoginButton = new JButton("Admin Login");
 		adminLoginButton.setBounds(presetx + 115, 80+ presety, 105, 25);
 		panel.add(adminLoginButton);
+		
+		userLoginButton.addActionListener(e -> {
+			String userID = userIDText.getText();
+			String password = userPWText.getText();
+			
+			boolean success = loginController.handleLogin(userID, password);
+			if(success) {//다른 뷰 생성 이후 연결 예정
+				JOptionPane.showMessageDialog(null, "로그인 성공!");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "등록되지 않은 ID나 잘못된 비밀번호를 입력하였습니다.");
+			}
+		});
 		
 	}
 
