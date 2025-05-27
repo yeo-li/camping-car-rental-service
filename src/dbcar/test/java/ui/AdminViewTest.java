5335258c6deb57ce8298d0cf069d90566c3832b6
Sunwoo <Administrator@swkim-ntb>
2025-05-27 10:02:12 +0900

    feat : 각 초기화면 단위테스트 구현 (#20)

--- src/dbcar/test/java/ui/AdminViewTest.java
+++ src/dbcar/test/java/ui/AdminViewTest.java
@@ -1,9 +1,11 @@
 package dbcar.test.java.ui;
 
 import dbcar.main.java.com.dbshindong.dbcar.ui.view.AdminInitPanel;
+import dbcar.main.java.com.dbshindong.dbcar.ui.coordinator.AppCoordinator;
 
 public class AdminViewTest {
 	public static void main(String[] args) {
-		new AdminInitPanel();
+		AppCoordinator coordinator = new AppCoordinator();
+		coordinator.showAdminInitView();
 	}
 }
--- src/dbcar/test/java/ui/UserInitViewTest.java
+++ src/dbcar/test/java/ui/UserInitViewTest.java
@@ -1,7 +1,9 @@
 package dbcar.test.java.ui;
+import dbcar.main.java.com.dbshindong.dbcar.ui.coordinator.AppCoordinator;
 import dbcar.main.java.com.dbshindong.dbcar.ui.view.UserInitPanel;
 public class UserInitViewTest {
 	public static void main(String[] args) {
-		new UserInitPanel();
+		AppCoordinator coordinator = new AppCoordinator();
+		coordinator.showUserInitView();
 	}
 }
