1763ce8d3b87301536b2dcc849612067c6a5de43
yeo-li <parkseongyeol2110@gmail.com>
2025-05-27 16:27:30 +0900

    test : SqlQueryView 테스트 코드 구현(#31)

--- src/dbcar/test/java/ui/SqlQueryViewTest.java
+++ src/dbcar/test/java/ui/SqlQueryViewTest.java
@@ -1,40 +1,18 @@
 package dbcar.test.java.ui;
 
-import dbcar.main.java.com.dbshindong.dbcar.application.DataFetchService;
-import dbcar.main.java.com.dbshindong.dbcar.application.DatabaseInitService;
-import dbcar.main.java.com.dbshindong.dbcar.infrastructure.*;
-import dbcar.main.java.com.dbshindong.dbcar.infrastructure.company.*;
-import dbcar.main.java.com.dbshindong.dbcar.infrastructure.customer.*;
-import dbcar.main.java.com.dbshindong.dbcar.infrastructure.repair.external.*;
-import dbcar.main.java.com.dbshindong.dbcar.infrastructure.repair.internal.*;
-import dbcar.main.java.com.dbshindong.dbcar.ui.controller.SqlQueryController;
-import dbcar.main.java.com.dbshindong.dbcar.ui.view.admin.SqlQueryView;
+import dbcar.main.java.com.dbshindong.dbcar.config.AppConfig;
+import dbcar.main.java.com.dbshindong.dbcar.ui.coordinator.AppCoordinator;
 
-import javax.swing.*;
-
-public class SqlQueryViewTestLauncher {
+public class SqlQueryViewTest {
 
 	public static void main(String[] args) {
-		// DB 연
-		new DBConnection("root", "1234");
-        DatabaseInitService initService = new DatabaseInitService();
-        initService.initDatabase(DBConnection.getConnection(), "dbcar/main/java/resources/DatabaseInit.sql");
+		AppCoordinator cd = new AppCoordinator();// dbconnection은 코디네이터에서 관리.
+		AppConfig ac = AppConfig.getInstance();
+		ac.dbConnection().setConnection("root", "1234");
+		ac.databaseInitService().initDatabase(ac.dbConnection().getConnection(),
+				"dbcar/main/java/resources/DatabaseInit.sql");
 
-
-		// 서비스 & 컨트롤러 생성
-		DataFetchService fetchService = new DataFetchService();
-
-		SqlQueryController controller = new SqlQueryController();
-		SqlQueryView view = new SqlQueryView();
-
-		// 프레임으로 띄우기
-		SwingUtilities.invokeLater(() -> {
-			JFrame frame = new JFrame("🎨 SQL View 화면 테스트");
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setSize(900, 600);
-			frame.add(view);
-			frame.setLocationRelativeTo(null); // 중앙 정렬
-			frame.setVisible(true);
-		});
+		cd.start();
+		cd.showSqlQueryView();
 	}
 }
