faa289ccda525bffa24453341d5bd66ff4ccdcf0
Sunwoo <Administrator@172.18.0.125>
2025-05-28 19:25:26 +0900

    feat : 패널, 컨트롤러 구현(#25)

--- src/dbcar/main/java/com/dbshindong/dbcar/ui/controller/CampingCarQueryController.java
+++ src/dbcar/main/java/com/dbshindong/dbcar/ui/controller/CampingCarQueryController.java
@@ -4,15 +4,21 @@ import java.util.List;
 
 import dbcar.main.java.com.dbshindong.dbcar.application.CampingCarQueryService;
 import dbcar.main.java.com.dbshindong.dbcar.domain.company.CampingCar;
+import dbcar.main.java.com.dbshindong.dbcar.ui.coordinator.AppCoordinator;
 
 public class CampingCarQueryController {
 	private final CampingCarQueryService campingCarQueryService;
+	private final AppCoordinator coordinator;
 	
-	public CampingCarQueryController(CampingCarQueryService campingCarQueryService) {
+	public CampingCarQueryController(CampingCarQueryService campingCarQueryService, AppCoordinator coordinator) {
 		this.campingCarQueryService = campingCarQueryService;
+		this.coordinator = coordinator;
 	}
 	
 	public List<CampingCar> handleQuery(){
 		return campingCarQueryService.CampingCarQuery();
+	}
+	public void onCarSelected(int id) {
+		this.coordinator.showCampingCarAvailableDateQueryView(id);
 	}
 }
