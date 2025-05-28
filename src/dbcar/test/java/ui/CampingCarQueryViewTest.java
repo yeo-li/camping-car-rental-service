f893e8805580380dfe2002686c677e85ac9b8ac3
Sunwoo <Administrator@172.18.0.125>
2025-05-28 19:26:19 +0900

    test : 캠핑카 조회 뷰 테스트 구현(#25)

--- src/dbcar/test/java/ui/CampingCarQueryViewTest.java
+++ src/dbcar/test/java/ui/CampingCarQueryViewTest.java
@@ -1,14 +1,15 @@
 package dbcar.test.java.ui;
 
-import dbcar.main.java.com.dbshindong.dbcar.ui.view.CampingCarQueryView;
 import dbcar.main.java.com.dbshindong.dbcar.application.CampingCarQueryService;
 import dbcar.main.java.com.dbshindong.dbcar.application.DatabaseInitService;
+import dbcar.main.java.com.dbshindong.dbcar.config.AppConfig;
 import dbcar.main.java.com.dbshindong.dbcar.infrastructure.DBConnection;
 import dbcar.main.java.com.dbshindong.dbcar.infrastructure.company.CampingCarRepository;
 import dbcar.main.java.com.dbshindong.dbcar.infrastructure.customer.RentalRepository;
 import dbcar.main.java.com.dbshindong.dbcar.infrastructure.repair.internal.PartRepository;
 import dbcar.main.java.com.dbshindong.dbcar.ui.controller.CampingCarAvailableDateQueryController;
 import dbcar.main.java.com.dbshindong.dbcar.ui.controller.CampingCarQueryController;
+import dbcar.main.java.com.dbshindong.dbcar.ui.coordinator.AppCoordinator;
 import dbcar.main.java.com.dbshindong.dbcar.application.CampingCarAvailableDateQueryService;
 public class CampingCarQueryViewTest {
 	
@@ -17,24 +18,13 @@ public class CampingCarQueryViewTest {
 	static PartRepository partRepository;
 	
 	public static void main(String[] args) {
-		
 		System.out.println("[[CampingCarQueryVeiwTest 초기 세팅]]");
+		System.out.println("\n[[CampingCarQueryTest]]");
+		AppConfig ac = AppConfig.getInstance();
 		
-		dc = new DBConnection("root", "1234");
-		databaseInitService = new DatabaseInitService();
-		partRepository = new PartRepository(DBConnection.getConnection());
-		databaseInitService.initDatabase(DBConnection.getConnection(), "dbcar/main/java/resources/DatabaseInit.sql");
-
-		System.out.println("\n[[CampingCarQueryVeiwTest]]");
-		CampingCarRepository campingCarRepository = new CampingCarRepository(DBConnection.getConnection()); 
-		CampingCarQueryService campingCarService = new CampingCarQueryService(campingCarRepository);
-		CampingCarQueryController campingCarQueryController = new CampingCarQueryController(campingCarService);
-		
-		
-		RentalRepository rentalRepository = new RentalRepository(DBConnection.getConnection()); 
-		CampingCarAvailableDateQueryService campingCarAvailableDateQueryService = new CampingCarAvailableDateQueryService(rentalRepository);
-		CampingCarAvailableDateQueryController campingCarAvailableDateQueryController = new CampingCarAvailableDateQueryController(campingCarAvailableDateQueryService);
-		
-		new CampingCarQueryView(campingCarQueryController, campingCarAvailableDateQueryController);
+		ac.dbConnection().setConnection("user1", "user1");
+		ac.databaseInitService().initDatabase(ac.dbConnection().getConnection(),"dbcar/main/java/resources/DatabaseInit.sql");
+		ac.appCoordinator().start();
+		ac.appCoordinator().showCampingCarQueryView();
 	}
 }
