67fd16b1b2df6b01c314b0d5fb98788cd8fbab1a
yeo-li <parkseongyeol2110@gmail.com>
2025-05-29 05:24:01 +0900

    feat : AppCoordinator 테이블 데이터 삭제/변경 화면 삽입(#35)

--- src/dbcar/main/java/com/dbshindong/dbcar/ui/coordinator/AppCoordinator.java
+++ src/dbcar/main/java/com/dbshindong/dbcar/ui/coordinator/AppCoordinator.java
@@ -40,6 +40,12 @@ public class AppCoordinator {
 		frame.revalidate();
 	}
 
+	public void showDeleteUpdateView() {
+		DeleteUpdatePanel view = new DeleteUpdatePanel();
+		frame.setContentPane(view);
+		frame.revalidate();
+	}
+
 	/*
 	 * public void showCampingCarQueryView(Customer user) { CampingCarRepository
 	 * campingCarRepository = new CampingCarRepository(this.conn);
