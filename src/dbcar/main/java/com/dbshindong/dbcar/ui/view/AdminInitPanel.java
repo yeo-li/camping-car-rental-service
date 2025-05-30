ed8af7c016f2f190085f536d31ef845024d047d4
yeo-li <parkseongyeol2110@gmail.com>
2025-05-30 15:28:06 +0900

    feat : 데이터베이스 초기화 화면 에러 핸들링(#34)

--- src/dbcar/main/java/com/dbshindong/dbcar/ui/view/AdminInitPanel.java
+++ src/dbcar/main/java/com/dbshindong/dbcar/ui/view/AdminInitPanel.java
@@ -2,6 +2,8 @@ package dbcar.main.java.com.dbshindong.dbcar.ui.view;
 
 import javax.swing.*;
 
+import dbcar.main.java.com.dbshindong.dbcar.common.exception.DatabaseException;
+import dbcar.main.java.com.dbshindong.dbcar.common.exception.GlobalExceptionHandler;
 import dbcar.main.java.com.dbshindong.dbcar.config.AppConfig;
 
 import java.awt.*;
@@ -43,16 +45,20 @@ public class AdminInitPanel extends JPanel {
 
 			int index = i; // 람다에서 사용하려면 final 또는 effectively final 변수 필요
 
-			button.addActionListener(e -> {
-				switch (index) {
-				case 0 -> showDatabaseResetConfirmation();
-				case 1 -> System.out.println("테이블 데이터 입력 화면으로 이동");
-				case 2 -> System.out.println("테이블 데이터 삭제/변경 화면으로 이동");
-				case 3 -> System.out.println("전체 테이블 보기 화면으로 이동");
-				case 4 -> System.out.println("캠핑카 정비 기록 조회 화면으로 이동");
-				case 5 -> System.out.println("SQL 질의 실행 및 결과 보기 화면으로 이동");
-				}
-			});
+			try {
+				button.addActionListener(e -> {
+					switch (index) {
+					case 0 -> showDatabaseResetConfirmation();
+					case 1 -> System.out.println("테이블 데이터 입력 화면으로 이동");
+					case 2 -> System.out.println("테이블 데이터 삭제/변경 화면으로 이동");
+					case 3 -> System.out.println("전체 테이블 보기 화면으로 이동");
+					case 4 -> System.out.println("캠핑카 정비 기록 조회 화면으로 이동");
+					case 5 -> System.out.println("SQL 질의 실행 및 결과 보기 화면으로 이동");
+					}
+				});
+			} catch (Exception e) {
+				GlobalExceptionHandler.handle(e);
+			}
 		}
 	}
 
