fede0b4db0395a723914b6b2bf7fa03a8d8c9823
Sunwoo <Administrator@swkim-ntb>
2025-05-30 19:30:17 +0900

    fix : 한글 정규식 추가 : (#61)

--- src/dbcar/main/java/com/dbshindong/dbcar/common/Validator.java
+++ src/dbcar/main/java/com/dbshindong/dbcar/common/Validator.java
@@ -1,7 +1,7 @@
 package dbcar.main.java.com.dbshindong.dbcar.common;
 
 public class Validator {
-	private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@" + // local-part
+	private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%가-힣]+@" + // local-part
 			"[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"; // domain
 
 	public static boolean isValidEmail(String email) {
