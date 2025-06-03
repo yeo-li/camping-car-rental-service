805e3ab9e2026c76638223d1f513e15bc75d0dc6
Sunwoo <Administrator@swkim-ntb>
2025-06-03 21:35:27 +0900

    feat : 프로그램 시작점 작성

--- src/dbcar/main/java/com/dbshindong/dbcar/DbCarApplication.java
+++ src/dbcar/main/java/com/dbshindong/dbcar/DbCarApplication.java
@@ -1,8 +1,23 @@
 package dbcar.main.java.com.dbshindong.dbcar;
 
+import dbcar.main.java.com.dbshindong.dbcar.application.DatabaseInitService;
+import dbcar.main.java.com.dbshindong.dbcar.config.AppConfig;
+import dbcar.main.java.com.dbshindong.dbcar.infrastructure.DBConnection;
+import dbcar.main.java.com.dbshindong.dbcar.infrastructure.repair.internal.PartRepository;
+
 public class DbCarApplication {
 
+	static DBConnection dc;
+	static DatabaseInitService databaseInitService;
+	static PartRepository partRepository;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("[프로그램 시작]");
+		
+		AppConfig ac = AppConfig.getInstance();
+		
+		ac.appCoordinator().start();
 	}
 }
+
